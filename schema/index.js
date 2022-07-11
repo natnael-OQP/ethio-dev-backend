97e2c69b77a00c83c0f1f5f3ef994e3d3f171bea
natnael-OQP <makiya112545@gmail.com>
2022-07-11 22:46:18 +0300

    create like schema

--- schema/index.js
+++ schema/index.js
@@ -4,14 +4,29 @@ exports.typeDefs = gql`
     type User {
         name: String!
         username: String!
+        email: String!
         photo: String
         coverImage: String
-        email: String
         age: Int
         bio: String
         isEmployee: String
         education: String
         work: String
+        links: [Link]
+        following: [Follow]
+        follower: [Follow]
+    }
+
+    type Link {
+        userId: ID!
+        name: String!
+        url: String!
+    }
+
+    type Follow {
+        userId: ID!
+        name: String!
+        url: String!
     }
 
     type Query {
